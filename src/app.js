23be81572bd3f8784388d2e1c91448e5000ce9b3
zjovic <38418292+zjovic@users.noreply.github.com>
2022-08-11 19:34:51 +0200

    Create excuse generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const who = ["The dog", "My grandma", "His turtle", "My bird"];
+const action = ["ate", "peed", "crushed", "broke"];
+const what = ["my homework", "the keys", "the car"];
+const when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const $el = document.querySelector("#excuse > h1");
+const excuseParts = [who, action, what, when];
+
+const randomNumGen = max => {
+  return Math.floor(Math.random() * (max + 1));
+};
+
+const generateExcuse = () => {
+  return excuseParts.map(part => part[randomNumGen(part.length - 1)]);
+};
+
+window.addEventListener("click", () => {
+  $el.innerHTML = generateExcuse().join(" ");
+});
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  $el.innerHTML = generateExcuse().join(" ");
 };
